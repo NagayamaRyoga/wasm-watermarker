a5c2ac0bd7887920a04fcad41b21d56244487ef4
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-03-08 18:15:33 +0900

    Add the setting file of clang-format.

--- src/kyut.cpp
+++ src/kyut.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	try
-	{
-		for (int i = 0; i < argc; i++)
-		{
-			std::cout << argv[i] << std::endl;
-		}
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << '\n';
+int main(int argc, char *argv[]) {
+    try {
+        for (int i = 0; i < argc; i++) {
+            std::cout << argv[i] << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
 
-		return 1;
-	}
+        return 1;
+    }
 }
