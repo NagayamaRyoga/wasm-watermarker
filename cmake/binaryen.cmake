ea02a066362455ef0f55b50d8569fbf35c66308c
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-03-13 16:01:00 +0900

    Fix cmake.

--- cmake/binaryen.cmake
+++ cmake/binaryen.cmake
@@ -4,18 +4,27 @@ ExternalProject_Add(
     PREFIX binaryen
     INSTALL_COMMAND ""
     TEST_COMMAND ""
-    LOG_DOWNLOAD ON
-    CMAKE_ARGS -DBUILD_STATIC_LIB=1
+    CMAKE_ARGS
+        -DBUILD_STATIC_LIB=ON
+        -DCMAKE_BUILD_TYPE=Release
 )
 
 ExternalProject_Get_Property(binaryen source_dir)
 ExternalProject_Get_Property(binaryen binary_dir)
 
-add_library(binaryen_wasm STATIC IMPORTED)
-add_dependencies(binaryen_wasm binaryen)
+file(MAKE_DIRECTORY ${source_dir}/src)
 
-set_target_properties(binaryen_wasm
+add_library(libbinaryen STATIC IMPORTED)
+add_dependencies(libbinaryen binaryen)
+
+set_target_properties(libbinaryen
     PROPERTIES
-    IMPORTED_LOCATION ${binary_dir}/lib/libwasm.a
+    IMPORTED_LOCATION ${binary_dir}/lib/libbinaryen.a
     INTERFACE_INCLUDE_DIRECTORIES ${source_dir}/src
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libwasm.a
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libir.a
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libcfg.a
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libpasses.a
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libsupport.a
+    INTERFACE_LINK_LIBRARIES ${binary_dir}/lib/libasmjs.a
 )
--- src/kyut.cpp
+++ src/kyut.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
+#include <wasm-io.h>
+
 int main(int argc, char *argv[]) {
     try {
-        for (int i = 0; i < argc; i++) {
-            std::cout << argv[i] << std::endl;
+        if (argc != 3) {
+            std::cerr << "WebAssembly digital watermarker." << std::endl;
+            std::cerr << "usage: kyut <input file> <watermark>" << std::endl;
+
+            return 1;
         }
+
+        wasm::Module module;
+        wasm::ModuleReader{}.readBinary(argv[1], module);
     } catch (const std::exception &e) {
         std::cerr << e.what() << '\n';
 
