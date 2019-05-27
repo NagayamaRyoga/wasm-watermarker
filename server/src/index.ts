e48162850688c85850c6da5e96c74747e9cca96f
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-05-27 20:45:21 +0900

    Stream wasm with watermarks

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
+import * as util from "util";
+import * as childProcess from "child_process";
 import * as fs from "fs";
 import * as express from "express";
 
 const port = 3000;
+const watermarker = `${__dirname}/../../build/src/kyut`;
 
 const app = express();
 
@@ -9,11 +12,19 @@ const server = app.listen(port, () => {
     console.log(`server listening at port ${port}`);
 });
 
-app.get("/wasm/add.wasm", (req: express.Request, res: express.Response) => {
-    const binary = fs.readFileSync(`${__dirname}/../wasm/add.wasm`);
+app.get(
+    "/wasm/add.wasm",
+    async (req: express.Request, res: express.Response) => {
+        const path = `${__dirname}/../wasm/add.wasm`;
+        const watermark = req.query.v || "";
 
-    res.writeHead(200, { "Content-Type": "application/wasm" });
-    res.end(binary);
-});
+        res.writeHead(200, { "Content-Type": "application/wasm" });
+
+        console.time("kyut");
+        const process = childProcess.spawn(watermarker, [path, watermark, "-"]);
+        process.stdout.pipe(res);
+        res.on("finish", () => console.timeEnd("kyut"));
+    }
+);
 
 app.use(express.static(`${__dirname}/../static`));
