025d42c5fc3398edf8f1b53a752fe77372ffe9d5
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-05-27 19:30:36 +0900

    Serve static files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import * as express from "express";
 
 const port = 3000;
@@ -8,6 +9,11 @@ const server = app.listen(port, () => {
     console.log(`server listening at port ${port}`);
 });
 
-app.get("/", (req: express.Request, res: express.Response) => {
-    res.send("Hello, world!");
+app.get("/wasm/add.wasm", (req: express.Request, res: express.Response) => {
+    const binary = fs.readFileSync(`${__dirname}/../wasm/add.wasm`);
+
+    res.writeHead(200, { contentType: "application/wasm" });
+    res.end(binary);
 });
+
+app.use(express.static(`${__dirname}/../static`));
