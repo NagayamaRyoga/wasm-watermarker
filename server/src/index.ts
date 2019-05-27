67780e69d3ff4ae97657276303dbaf437bce7164
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-05-27 19:16:36 +0900

    Add simplest response

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,3 +7,7 @@ const app = express();
 const server = app.listen(port, () => {
     console.log(`server listening at port ${port}`);
 });
+
+app.get("/", (req: express.Request, res: express.Response) => {
+    res.send("Hello, world!");
+});
