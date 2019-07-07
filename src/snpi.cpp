c419a4f504a8c5550c8cf01b6a2fbc1e43984dd0
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-07-08 01:12:57 +0900

    feature: Output the result

--- src/snpi.cpp
+++ src/snpi.cpp
@@ -16,9 +16,20 @@ int main(int argc, char *argv[]) {
         // Read the input module
         wasm::Module module;
         wasm::ModuleReader{}.read(inputFile, module);
+
+        // Output the result
+        wasm::ModuleWriter{}.writeText(module, "");
     } catch (const wasm::ParseException &e) {
         e.dump(std::cerr);
+
+        return 1;
+    } catch (const std::exception &e) {
+        fmt::print(std::cerr, "exception: {}\n", e.what());
+
+        return 1;
     } catch (...) {
         fmt::print(std::cerr, "unknown exception\n");
+
+        return 1;
     }
 }
