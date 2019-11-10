4746dc8b366f01ac3eb8b08545c6b7bcbc665351
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-11-10 18:27:13 +0900

    Implement bench

--- example/zlib-wasm/benchmark.js
+++ example/zlib-wasm/benchmark.js
@@ -1,10 +1,37 @@
-(async function() {
-  const Zlib = await require('./zlib').initialize('./zlib-sample.wasm');
+const Crypto = require('crypto');
+const assert = require('assert');
 
-  const rawData = Buffer.from([...Array(10000).keys()]);
-  const compressedData = Zlib.deflate(rawData);
-  const uncompressedData = Zlib.inflate(compressedData);
+const size = 1_000_000;
+const rawData = Crypto.randomBytes(size);
 
-  console.log(uncompressedData.equals(rawData));
+(async function () {
+  const libPaths = [
+    './zlib-sample.wasm',
+  ];
+
+  const libs = await Promise.all(libPaths.map(async path => await require('./zlib').initialize(path)));
+
+  // Validation
+  libs.forEach((Zlib, i) => {
+    assert.deepEqual(Zlib.inflate(Zlib.deflate(rawData)), rawData, libPaths[i]);
+  });
+
+  // Warming up
+  libs.forEach((Zlib, i) => {
+    console.time(`Warm - ${libPaths[i]}`);
+    for (let i = 0; i < 5; i++) {
+      Zlib.deflate(rawData);
+    }
+    console.timeEnd(`Warm - ${libPaths[i]}`);
+  });
+
+  // Benchmark
+  libs.forEach((Zlib, i) => {
+    console.time(`Benchmark - ${libPaths[i]}`);
+    for (let i = 0; i < 100; i++) {
+      Zlib.deflate(rawData);
+    }
+    console.timeEnd(`Benchmark - ${libPaths[i]}`);
+  });
 }());
 
