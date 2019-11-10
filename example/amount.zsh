2c9e8b0ed3cee84f3cc2403b99635aed5fca11f9
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-11-10 21:07:49 +0900

    Add an aggregation script

---
+++ example/amount.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+path=("$PWD/../build/src" $path[@])
+
+wasms=(
+    $PWD/styled-jsx/mappings.wasm
+    $PWD/zlib-wasm/zlib-sample.wasm
+    $PWD/ammo.wasm.wasm
+)
+
+echo "name, size[B], funcord[B], opswap[B]"
+for i in $wasms[@]
+do
+    size="$(stat -f%z "$i")"
+    funcord="$((snpi "$i" funcord "Test" > /dev/null) |& awk '{print $1}')"
+    opswap="$((snpi "$i" opswap "Test" > /dev/null) |& awk '{print $1}')"
+    echo "${i/$PWD\//}, $size, $funcord, $opswap"
+done
