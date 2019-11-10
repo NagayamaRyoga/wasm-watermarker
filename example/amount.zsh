3307959dabf634eca6f985765082981baaae199a
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2019-11-10 21:16:39 +0900

    Update the aggregation script

--- example/amount.zsh
+++ example/amount.zsh
@@ -7,11 +7,12 @@ wasms=(
     $PWD/ammo.wasm.wasm
 )
 
-echo "name, size[B], funcord[B], opswap[B]"
+echo "name, funcs, size[B], funcord[B], opswap[B]"
 for i in $wasms[@]
 do
     size="$(stat -f%z "$i")"
+    funcs="$(funccnt "$i")"
     funcord="$((snpi "$i" funcord "Test" > /dev/null) |& awk '{print $1}')"
     opswap="$((snpi "$i" opswap "Test" > /dev/null) |& awk '{print $1}')"
-    echo "${i/$PWD\//}, $size, $funcord, $opswap"
+    echo "${i/$PWD\//}, $size, $funcs, $funcord, $opswap"
 done
