cb4e06e278081320f304e8a3d0abd7026d124f20
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2021-01-08 15:37:42 +0900

    add an extra parameter to import functions

--- src/kyuk.cpp
+++ src/kyuk.cpp
@@ -1,10 +1,46 @@
 #include <fmt/printf.h>
 #include "cmdline.h"
 #include "wasm-io.h"
+#include "wasm-validator.h"
 
 namespace {
     const std::string program_name = "kyuk";
     const std::string version = "0.1.0";
+
+    void add_parameter(wasm::Function& f) {
+        wasm::Type new_param{wasm::Type::i32};
+
+        switch (f.sig.params.getID()) {
+            case wasm::Type::none:
+                // No parameter
+                f.sig.params = new_param;
+                break;
+
+            case wasm::Type::i32:
+            case wasm::Type::i64:
+            case wasm::Type::f32:
+            case wasm::Type::f64:
+            case wasm::Type::v128:
+                // 1 primitive parameter
+                f.sig.params = wasm::Type{f.sig.params, new_param};
+                break;
+
+            default:
+                if (f.sig.params.isTuple()) {
+                    // 2 or more parameters
+                    wasm::Tuple tuple{};
+                    for (const auto& t : f.sig.params) {
+                        tuple.types.emplace_back(t);
+                    }
+                    tuple.types.emplace_back(new_param);
+                    f.sig.params = wasm::Type{tuple};
+                } else {
+                    // May not be reached
+                    throw std::runtime_error{"unknown parameter type"};
+                }
+                break;
+        }
+    }
 } // namespace
 
 int main(int argc, char* argv[]) {
@@ -60,7 +96,26 @@ int main(int argc, char* argv[]) {
         wasm::Module module{};
         wasm::ModuleReader{}.read(input, module);
 
-        // TODO: Embedding
+        // Embedding
+        // TODO: Add extra arguments to import function callings
+
+        // Add extra parameters to import functions
+        for (const auto& f : module.functions) {
+            if (f->body != nullptr) {
+                continue;
+            }
+
+            // `f` is an import function
+            fmt::print("f: {}, {}\n", f->name, f->sig.params.toString());
+            add_parameter(*f);
+
+            fmt::print("f: {}, {}\n", f->name, f->sig.params.toString());
+        }
+
+        // Validation
+        if (!wasm::WasmValidator{}.validate(module)) {
+            std::exit(EXIT_FAILURE);
+        }
 
         wasm::ModuleWriter w{};
         w.setDebugInfo(preserve_debug);
